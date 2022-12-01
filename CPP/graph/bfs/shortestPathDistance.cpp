0b93e7d1322344b635fb9f6283191d786e7aa39f
nur-alam <nuralam862@gmail.com>
2022-12-01 14:48:08 +0600

    add input output

--- CPP/graph/bfs/shortestPathDistance.cpp
+++ CPP/graph/bfs/shortestPathDistance.cpp
@@ -82,9 +82,21 @@ int main() {
     }
     bfs(g, node);
     getNodesDistance(node);
-    getPath(8);
+    getPath(5);
     // printGraph(g, node);
     // cout << endl << "yo" << endl;
 
     return 0;
 }
+
+// input 
+// 6 6
+// 0 1
+// 0 2
+// 0 3
+// 2 4
+// 2 5
+// 3 5
+// output
+// 0 1 1 1 2 2 
+// 0 2 5 
