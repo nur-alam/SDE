b2e02dc35c36c69a5afda7a9a04fc0045af14ce6
nur-alam <nuralam862@gmail.com>
2022-12-15 21:36:10 +0600

    reverse Delete Mst

---
+++ CPP/stl/lamda func/lamda.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+void printVector(vector<int> v) {
+    // lambda expression to print vector
+    for_each(v.begin(), v.end(), [](int i)
+    {
+        std::cout << i << " ";
+    });
+    cout << endl;
+}
+
+
+int main() {
+
+    vector<int> v {4, 1, 3, 5, 2, 3, 1, 7};
+
+    printVector(v);
+
+    // sort(v.begin(), v.end(), [](int a, int b) {
+    //     // cout << a << " " << b << " " << endl;
+    //     return a == b;
+    // });
+
+    p = unique(v.begin(), v.end(), [](int a, int b)
+    {
+        return a == b;
+    });
+ 
+    // resizing vector to make size equal to total different number
+    v.resize(distance(v.begin(), p));
+    printVector(v);
+
+    printVector(v);
+
+    return 0;
+}
