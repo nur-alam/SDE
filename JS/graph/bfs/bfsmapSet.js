0dbc9fec195790b4700286ec1423db60a7daaab2
nur-alam <nuralam862@gmail.com>
2023-03-26 17:28:02 +0600

    bfs implement with Map Set of js with adjList

---
+++ JS/graph/bfs/bfsmapSet.js
@@ -0,0 +1,66 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.trim().split('\n').map( string => string.trim() );
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function printGraph(adj) {
+    for (let i = 0; i < adj.length; i++) {
+        let arr = [];
+        for (let v of adj[i]) {
+            arr.push(v);
+        }
+        console.log(arr);
+    }
+}
+
+function bfs(N, adj, startNode) {
+    const visited = new Set();
+    const q = [];
+    q.push(startNode);
+    visited.add(startNode);
+    while (q.length) {
+        const currentNode = q.shift();
+        visited.add(startNode);
+        for (let neighbour of adj.get(currentNode)) {
+            if (!visited.has(neighbour)) {
+                q.push(neighbour);
+                visited.add(neighbour);
+            }
+        }
+    }
+    console.log(visited);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    for (let i = 0; i < t; i++) {
+        let [N, E] = readLine().split(' ');
+        let adj = new Map();
+        for (let i = 0; i < N; i++) {
+            adj.set(i, new Set());
+        }
+        for (let i = 0; i < E; i++) {
+            let input_line = readLine().split(' ');
+            let u = parseInt(input_line[0]);
+            let v = parseInt(input_line[1]);
+            adj.get(u).add(v);
+        }
+        // console.log(adj);
+        bfs(N, adj, 0);
+    }
+}
--- JS/graph/bfs/test.js
+++ JS/graph/bfs/test.js
@@ -1,4 +1,9 @@
-let yo = new Array(10).fill(0);
-if (yo.length) {
-    console.log(yo.length);
+// let yo = new Array(10).fill(0);
+let adj = new Map();
+for (let i = 0; i < 3; i++) {
+    adj.set(i, new Set());
 }
+adj.get(0).add(2);
+adj.get(0).add(2);
+adj.get(0).add(1);
+console.log(adj.get(0));
